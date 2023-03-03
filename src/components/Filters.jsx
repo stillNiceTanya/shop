569106cf2f430202a27ae5c8fcffe57af67c1e61
TatianaP <tanya.email4work@gmail.com>
2023-03-03 22:35:41 +0700

    Add debounce to filtering

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,6 +3,7 @@ import { useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import { debounce } from "throttle-debounce";
 
 import { setFilter } from "../redux/actions/setFilter";
 import Select from "./Select";
@@ -11,11 +12,15 @@ import InputSearch from "./InputSearch";
 export default function Filters({ products }) {
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    return () =>
+      dispatch(setFilter({ category: undefined, search: undefined }));
+  }, []);
+
   const categories = useSelector((state) => state.category);
   const prices = useSelector((state) =>
     state.products.data.map((el) => el.price)
   );
-
   const searchString = useSelector((state) => state.filters.search);
   const minPrice = useSelector((state) => state.filters.minPrice);
   const maxPrice = useSelector((state) => state.filters.maxPrice);
@@ -32,10 +37,10 @@ export default function Filters({ products }) {
 
   const defoultPriceFilter = [minProductPrice, maxProductPrice];
 
-  useEffect(() => {
-    return () =>
-      dispatch(setFilter({ category: undefined, search: undefined }));
-  }, []);
+  const isEmptyCatalog = useMemo(
+    () => searchString && products.length === 0,
+    [searchString, products]
+  );
 
   const handleFilterCategoryChange = (event) => {
     dispatch(setFilter({ category: event.target.value }));
@@ -45,15 +50,14 @@ export default function Filters({ products }) {
     dispatch(setFilter({ search: event.target.value }));
   };
 
-  const isEmptyCatalog = useMemo(
-    () => searchString && products.length === 0,
-    [searchString, products]
-  );
+  const debounceSearch = debounce(500, handleInputSearch);
 
-  const handleFilerPriceChange = (values) => {
+  function handleFilerPriceChange(values) {
     dispatch(setFilter({ minPrice: values[0] }));
     dispatch(setFilter({ maxPrice: values[1] }));
-  };
+  }
+
+  const debounceChange = debounce(500, handleFilerPriceChange);
 
   return (
     <form className="flex flex-col mr-9 gap-9">
@@ -62,7 +66,7 @@ export default function Filters({ products }) {
         <span>
           {isEmptyCatalog ? "try another search" : "what're you looking for?"}
         </span>
-        <InputSearch onChange={handleInputSearch} />
+        <InputSearch onChange={debounceSearch} />
       </div>
 
       <Select
@@ -75,7 +79,7 @@ export default function Filters({ products }) {
           range
           min={minProductPrice}
           max={maxProductPrice}
-          onChange={handleFilerPriceChange}
+          onChange={debounceChange}
           defaultValue={defoultPriceFilter}
           trackStyle={{ backgroundColor: "#D8D8D8" }}
           handleStyle={{
