d4caabe37874b6d44a867ee53cb2632661152411
TatianaP <tanya.email4work@gmail.com>
2023-03-27 13:26:03 +0800

    Allocated a block with buttons to a separate module

---
+++ src/components/ProductCard/AddToCartContainer.jsx
@@ -0,0 +1,80 @@
+import { useState, useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import classNames from 'classnames';
+
+import { addToCart } from '../../redux/actions/addProductToCart';
+import { updateCart } from '../../redux/actions/addProductToCart';
+import helpers from '../../helpers';
+
+export default function AddToCartContainer({ product }) {
+  const dispatch = useDispatch();
+
+  const [count, setCount] = useState(1);
+
+  const cart = useSelector((state) => state.cart);
+
+  const isInCart = useMemo(
+    () => cart.some((el) => el.id === product.id),
+    [cart, product],
+  );
+
+  const handleAddToCart = useCallback(() => {
+    const productData = {
+      id: product.id,
+      price: product.price,
+      quantity: count,
+      name: product.title,
+    };
+    if (isInCart) {
+      dispatch(updateCart(productData));
+      helpers.showInfoMessage(
+        `you have successfully added to the cart ${count} items`,
+      );
+      return;
+    }
+    dispatch(addToCart(productData));
+    helpers.showSuccessMessage(`${product.title} was added to the cart`);
+  }, [product, count, dispatch, isInCart]);
+
+  const handleDecrement = useCallback(() => {
+    if (count === 1) return;
+
+    setCount(count - 1);
+  }, [count]);
+
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  const handleInputChange = (event) => {
+    const newValue = parseInt(event.target.value);
+    if (!isNaN(newValue)) {
+      setCount(newValue);
+    }
+  };
+
+  return (
+    <>
+      <div className="hidden lg:block">
+        <button
+          className={classNames('mr-4', {
+            'cursor-not-allowed': count === 1,
+          })}
+          onClick={handleDecrement}
+        >
+          -
+        </button>
+        <input
+          className="w-8"
+          type="text"
+          value={count}
+          onChange={handleInputChange}
+        />
+        <button className="cursor-pointer ml-4" onClick={handleIncrement}>
+          +
+        </button>
+      </div>
+      <button onClick={handleAddToCart}>ADD TO CART</button>
+    </>
+  );
+}
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,65 +1,13 @@
-import { useEffect, useMemo, useCallback, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { ToastContainer, toast } from 'react-toastify';
-import classNames from 'classnames';
+import { ToastContainer } from 'react-toastify';
 
-import { addToCart } from '../../redux/actions/addProductToCart';
-import { updateCart } from '../../redux/actions/addProductToCart';
-
-import helpers from '../../helpers';
+import AddToCartContainer from './AddToCartContainer';
 
 import 'react-toastify/dist/ReactToastify.css';
 
+//TODO разбить все на модули. button block вынести, походить подумать где еще что вынести
 //TODO при перезагрузке страницы стор сбрасывается - класть в локал сторадж или куки
-//TODO выскакивает ошибка файла конфигурации в девтулзах. Валидность json проверила, что делать?
 
 export default function ProductCard({ product }) {
-  const dispatch = useDispatch();
-
-  const [count, setCount] = useState(1);
-
-  const cart = useSelector((state) => state.cart);
-
-  const isInCart = useMemo(
-    () => cart.some((el) => el.id === product.id),
-    [cart, product],
-  );
-
-  const handleAddToCart = useCallback(() => {
-    const productData = {
-      id: product.id,
-      price: product.price,
-      quantity: count,
-      name: product.title,
-    };
-    if (isInCart) {
-      dispatch(updateCart(productData));
-      helpers.showInfoMessage(
-        `you have successfully added to the cart ${count} items`,
-      );
-      return;
-    }
-    dispatch(addToCart(productData));
-    helpers.showSuccessMessage(`${product.title} was added to the cart`);
-  }, [product, count, dispatch, isInCart]);
-
-  const handleDecrement = useCallback(() => {
-    if (count === 1) return;
-
-    setCount(count - 1);
-  }, [count]);
-
-  const handleIncrement = useCallback(() => {
-    setCount(count + 1);
-  }, [count]);
-
-  const handleInputChange = (event) => {
-    const newValue = parseInt(event.target.value);
-    if (!isNaN(newValue)) {
-      setCount(newValue);
-    }
-  };
-
   return (
     <div className="flex flex-col lg:max-w-484 md:w-1/2">
       <>
@@ -74,29 +22,8 @@ export default function ProductCard({ product }) {
       <span className="font-normal leading-7 mb-12 text-base text-darkGray-100 ">
         {product.description}
       </span>
-      <div className="lg:flex lg:justify-between ">
-        <div className="hidden lg:block">
-          <button
-            className={classNames('mr-4', {
-              'cursor-not-allowed': count === 1,
-            })}
-            onClick={handleDecrement}
-          >
-            -
-          </button>
-
-          <input
-            className="w-8"
-            type="text"
-            value={count}
-            onChange={handleInputChange}
-          />
-
-          <button className="cursor-pointer ml-4" onClick={handleIncrement}>
-            +
-          </button>
-        </div>
-        <button onClick={handleAddToCart}>ADD TO CART</button>
+      <div className="lg:flex lg:justify-between">
+        <AddToCartContainer product={product} />
       </div>
       <ToastContainer />
     </div>
