fd122ce1ca120456f29ae146bf99405866a40048
TatianaP <tanya.email4work@gmail.com>
2023-03-28 13:31:52 +0800

    Add localStorage for cart

---
+++ src/redux/actions/setInitialCart.js
@@ -0,0 +1,6 @@
+import { SET_INITIAL_CART } from '../actionTypes';
+
+export const setInitialCart = (data) => ({
+  type: SET_INITIAL_CART,
+  data,
+});
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,24 +1,49 @@
-import { ADD_PRODUCT_TO_CART, UPDATE_PRODUCT_IN_CART } from '../actionTypes';
+import {
+  ADD_PRODUCT_TO_CART,
+  UPDATE_PRODUCT_IN_CART,
+  SET_INITIAL_CART,
+} from '../actionTypes';
 
-const initialState = [];
+const initialState = { isAdded: false, data: [] };
 
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PRODUCT_TO_CART: {
-      return [...state, action.data];
+      return {
+        ...state,
+        data: [...state.data, action.data],
+        isAdded: true,
+      };
     }
 
     case UPDATE_PRODUCT_IN_CART: {
       const { id, quantity } = action.data;
-      return state.map((product) => {
-        if (product.id === id) {
-          return {
-            ...product,
-            quantity: product.quantity + quantity,
-          };
-        }
-        return product;
-      });
+      const productIndex = state.data.findIndex((product) => product.id === id);
+
+      if (productIndex !== -1) {
+        const updatedProduct = {
+          ...state.data[productIndex],
+          quantity: state.data[productIndex].quantity + quantity,
+        };
+        const newData = [...state.data];
+        newData[productIndex] = updatedProduct;
+        return {
+          ...state,
+          data: newData,
+          isAdded: true,
+        };
+      }
+      return state;
+    }
+
+    case SET_INITIAL_CART: {
+      if (Array.isArray(action.data)) {
+        return {
+          data: [...action.data],
+          isAdded: true,
+        };
+      }
+      return state;
     }
 
     default:
